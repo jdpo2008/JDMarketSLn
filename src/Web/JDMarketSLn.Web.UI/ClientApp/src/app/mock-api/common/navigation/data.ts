51a10319951f88bf27d323187bbde587455be910
Jose Daniel <jdpo2008@gmail.com>
2022-07-18 14:14:15 -0500

    JDP: se realzian cambios en el diseño

--- src/Web/JDMarketSLn.Web.UI/ClientApp/src/app/mock-api/common/navigation/data.ts
+++ src/Web/JDMarketSLn.Web.UI/ClientApp/src/app/mock-api/common/navigation/data.ts
@@ -3,11 +3,67 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
+        id: 'pages',
+        title: 'Paginas',
+        type: 'group',
+        //icon: 'heroicons_outline:chart-pie',
+        //link: '/dashboard',
+    },
+    {
         id: 'dashboard',
         title: 'Dashboard',
         type: 'basic',
         icon: 'heroicons_outline:chart-pie',
         link: '/dashboard',
+    },
+    {
+        id: 'seguridad',
+        title: 'Seguridad',
+        type: 'group',
+        //icon: 'heroicons_outline:chart-pie',
+        //link: '/dashboard',
+    },
+    {
+        id: 'usuarios',
+        title: 'Users',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/seguridad/users',
+    },
+    {
+        id: 'roles',
+        title: 'Roles',
+        type: 'basic',
+        icon: 'heroicons_outline:key',
+        link: '/seguridad/roles',
+    },
+    {
+        id: 'mantenimiento',
+        title: 'Mantenimiento',
+        type: 'group',
+        //icon: 'heroicons_outline:chart-pie',
+        //link: '/dashboard',
+    },
+    {
+        id: 'products',
+        title: 'Products',
+        type: 'basic',
+        icon: 'heroicons_outline:beaker',
+        link: '/mantenimiento/products',
+    },
+    {
+        id: 'categories',
+        title: 'Categories',
+        type: 'basic',
+        icon: 'heroicons_outline:adjustments',
+        link: '/mantenimiento/categories',
+    },
+    {
+        id: 'supliers',
+        title: 'Supliers',
+        type: 'basic',
+        icon: 'heroicons_outline:newspaper',
+        link: '/mantenimiento/supliers',
     },
 ];
 export const compactNavigation: FuseNavigationItem[] = [
